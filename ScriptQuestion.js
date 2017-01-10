a12387092e741b1e47e90935318cd62d5f7e83c0
vincrante <vincrante31@gmail.com>
2017-01-10 19:22:53 +0100

    Page creation question + script reste l'option des modif a faire

---
+++ ScriptQuestion.js
@@ -0,0 +1,13 @@
+/**
+ * Created by vincent on 10/01/17.
+ */
+var index = 3
+$(document).ready(function(){
+    $("#more").click(function () {
+        var large ='<tr><td>Réponse '+index+' : <input type="text" name = "reponse'+index+'"></td><td>Vrai ? : <input type="checkbox" name="check'+index+'"></td><td>FeedBack : <input type="text" name="feedback'+index+'" ></td></tr>'
+        $("#reponse").append(large),
+        index++,
+         $("#index").val(index-1),
+        console.log(index)
+    });
+});
--- question.php
+++ question.php
@@ -1,7 +1,85 @@
 <?php
+include('PDO.php');
+if (isset($_POST['insertQuestion']) && $_POST['insertQuestion'] == 'valider')
+{
+
+
+    $data = $monPDO->prepare('INSERT INTO question (`question`,`theme`) 
+                              VALUES ("'.$_POST['question'].'","'.$_POST['theme'].'")');
+    $data->execute();
+    $lastQuest = $monPDO->lastInsertId();
+        //$res = $data->fetch();
+    $index = $_POST['index'];
+    $reponse = 'INSERT INTO reponse (reponse,juste,feedBack) VALUES ';
+    for ($i = 1; $i < $index ; $i++){
+        $bool = FALSE;
+        if(isset($_POST['check'.$i])){
+            $bool = TRUE;
+        }
+
+        $reponse = $reponse.'("'.$_POST['reponse'.$i].'","'.$bool.'","'.$_POST['feedback'.$i].'"),';
+    }
+    $bool = FALSE;
+    if(isset($_POST['check'.$index])){
+        $bool = TRUE;
+    }
+
+    $reponse = $reponse.'("'.$_POST['reponse'.$index].'","'.$bool.'","'.$_POST['feedback'.$index].'")';
+    echo $reponse;
+    $dataReponse = $monPDO->prepare($reponse);
+    $dataReponse->execute();
+    $lastRep = $monPDO->lastInsertId();
+
+    $assoQR = 'INSERT INTO assoQuestReponse(idQuestion,idReponse) VALUES ';
+    for ($y = 0; $y < $index-1; $y++){
+        $assoQR = $assoQR.' ('.$lastQuest.','.(intval($lastRep) + $y).'),';
+    }
+    $assoQR = $assoQR.' ('.$lastQuest.','.(intval($lastRep) + $index-1).')';
+
+    $dataAsso = $monPDO->prepare($assoQR);
+    echo $assoQR;
+    $dataAsso->execute();
+
+
+    echo "<br/>Question enregistré<br/>";
+}
 /**
  * Created by PhpStorm.
  * User: vincent
  * Date: 10/01/17
  * Time: 10:41
  */
+
+
+?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<html>
+<head>
+<title>Question</title>
+</head>
+
+<body>
+Question :<br />
+<form action="question.php" method="post">
+    question : <input type="text" name="question" value="<?php if (isset($_POST['question'])) echo htmlentities(trim($_POST['question'])); ?>"><br />
+    Theme : <input type="text" name="theme" value="<?php if (isset($_POST['theme'])) echo htmlentities(trim($_POST['theme'])); ?>"><br />
+
+    <table id="reponse">
+       <tr>Réponse</tr>
+        <tr>
+            <td>Réponse 1 : <input type="text" name = "reponse1"></td>
+            <td>Vrai ? : <input type="checkbox" name="check1"></td>
+            <td>FeedBack : <input type="text" name="feedback1" value="<?php if (isset($_POST['feedBack'])) echo htmlentities(trim($_POST['feedBack'])); ?>"></td>
+        </tr>
+        <tr>
+            <td>Réponse 2 : <input type="text" name = "reponse2"></td>
+            <td>Vrai ? : <input type="checkbox" name="check2"></td>
+            <td>FeedBack : <input type="text" name="feedback2" value="<?php if (isset($_POST['feedBack'])) echo htmlentities(trim($_POST['feedBack'])); ?>"></td>
+        </tr>
+
+    </table>
+        <input name ="index" id="index" type="hidden" value="2"/>
+    <div id="more">Ajouter une reponse</div>
+    <input type="submit"  name="insertQuestion" value="valider">
+</form>
+<script src="ScriptQuestion.js"></script>
