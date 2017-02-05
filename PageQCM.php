0adec318244042b20feaf91241437ca8e52d77fc
Saykoh <corentin.banquet0155@gmail.com>
2017-02-05 14:19:21 +0100

    Modification Formulaire

--- PageQCM.php
+++ PageQCM.php
@@ -9,7 +9,7 @@ if(isset($_POST['begin']))
                           . 'AND QC.nom ="'.$_POST["begin"].'"');
     $reqQuest->execute();
     $tabResQuest = $reqQuest->fetchAll();
-    
+echo('<form name="'.$_POST["begin"].'" method="POST" action="pageResultat.php">');    
     foreach($tabResQuest as $resQuest)
     {
 ?>
@@ -27,22 +27,20 @@ if(isset($_POST['begin']))
                 $reqRep->execute();
                 $tabResRep = $reqRep->fetchAll();
             ?>
-            <form>
+
             <?php
             foreach($tabResRep as $resRep)
             {
-                echo('<input type="radio" value="'.$resRep['reponse'].'"/>'.$resRep['reponse']);
+                echo('<input type="radio" name="'.$resQuest['question'].'" value="'.$resRep['reponse'].'"/>'.$resRep['reponse']);
                 echo('<br/>');
             }
             ?>
-            </form>
         </td>
     </tr>
 </table>
 <?php
     }
 echo('<br/>');
-echo ('<form method="POST" action="pageResultat.php">');
 echo ('<input type="hidden" name="qcm" value ="'.$_POST['begin'].'"/>');
 echo ('<input type="submit" name="valid" value="Valider"/>');
 echo ('</form>');
